3eeab563b72d665790dfc9bf48500e3f75419962
PhaniKumar761 <x21175098@student.ncirl.ie>
2023-04-28 01:14:07 +0100

    Added a library function

--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
+import sys
 from django.shortcuts import render
+from django.shortcuts import render
+from Mquote.Mquote import generate_quote
 
+print(sys.executable)
 # the home page
 def home(request):
-    return render(request, "homePage/index.html", {})
+    token = generate_quote()
+    print(token)
+    return render(request, "homePage/index.html", {'token': token})
+   
